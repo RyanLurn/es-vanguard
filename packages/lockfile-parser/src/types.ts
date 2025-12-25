c8c6846ad2a0a3f075320c691132cff870c0a6bf
Ryan Luong <lumintriet@gmail.com>
2025-12-25 07:37:08 +0700

    refactor(lockfile-parser): remove legacy code.

--- packages/lockfile-parser/src/types.ts
+++
@@ -1,6 +0,0 @@
-import type { DependenciesSchema } from "@/schemas";
-import type * as z from "zod";
-
-type ParsedDependencies = z.infer<typeof DependenciesSchema>;
-
-export type { ParsedDependencies };
