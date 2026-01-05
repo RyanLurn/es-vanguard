298b6ba276f6d9446d56fbe915ad8b6c48da3b1b
Ryan Luong <lumintriet@gmail.com>
2026-01-05 09:53:18 +0700

    feat(manager): configure port for dev.

--- services/manager/src/index.ts
+++ services/manager/src/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
+import { MANAGER_SERVICE_DEV_PORT } from "@es-vanguard/utils/constants/ports";
 
 const app = new Hono();
 app.get("/", (c) => c.text("Hello Bun!"));
 
-export default app;
+export default {
+  port: process.env.PORT ? Number(process.env.PORT) : MANAGER_SERVICE_DEV_PORT,
+  fetch: app.fetch,
+};
