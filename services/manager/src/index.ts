48f87058979adda37a62a21ed72875d27c063261
Ryan Luong <lumintriet@gmail.com>
2026-01-05 09:57:57 +0700

    feat(manager): watch report endpoint.

--- services/manager/src/index.ts
+++ services/manager/src/index.ts
@@ -2,7 +2,9 @@ import { Hono } from "hono";
 import { MANAGER_SERVICE_DEV_PORT } from "@es-vanguard/utils/constants/ports";
 
 const app = new Hono();
-app.get("/", (c) => c.text("Hello Bun!"));
+app.get("/", (c) => c.text("Hi, I'm the Manager service!"));
+
+app.post("/watch-report", (c) => c.text("Watch report endpoint hit!"));
 
 export default {
   port: process.env.PORT ? Number(process.env.PORT) : MANAGER_SERVICE_DEV_PORT,
