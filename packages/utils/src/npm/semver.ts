54a13b0206a592222b15222602a1bbd7bbc2e570
Ryan Luong <lumintriet@gmail.com>
2026-01-09 08:42:07 +0700

    refactor(utils/npm): change semver default import into indivitual function imports.

--- packages/utils/src/npm/semver.ts
+++ packages/utils/src/npm/semver.ts
@@ -1,9 +1,10 @@
-import semver from "semver";
+import semverValid from "semver/functions/valid";
+import semverValidRange from "semver/ranges/valid";
 import * as z from "zod";
 
 export const SemverSchema = z
   .stringFormat("semver", (value) => {
-    return semver.valid(value);
+    return semverValid(value);
   })
   .brand("semver")
   .brand("semver-range");
@@ -11,7 +12,7 @@ export type Semver = z.infer<typeof SemverSchema>;
 
 export const SemverRangeSchema = z
   .stringFormat("semver-range", (value) => {
-    return semver.validRange(value);
+    return semverValidRange(value);
   })
   .brand("semver-range");
 export type SemverRange = z.infer<typeof SemverRangeSchema>;
