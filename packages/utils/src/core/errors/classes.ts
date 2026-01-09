91567dc4154335cb5ebcdf204778cd5c3173f1a0
Ryan Luong <lumintriet@gmail.com>
2026-01-09 17:26:55 +0700

    feat(utils/core/errors): add validation error class.

--- packages/utils/src/core/errors/classes.ts
+++ packages/utils/src/core/errors/classes.ts
@@ -1,3 +1,5 @@
+import { prettifyError, type ZodError } from "zod";
+
 export class UnexpectedError extends Error {
   constructor(message: string, options?: ErrorOptions) {
     super(message, options);
@@ -9,5 +11,15 @@ export class ExoticError extends UnexpectedError {
   constructor(message: string, options?: ErrorOptions) {
     super(message, options);
     this.name = "ExoticError";
+  }
+}
+
+export class ValidationError extends Error {
+  issues: ZodError["issues"];
+
+  constructor(zodError: ZodError) {
+    super(prettifyError(zodError), { cause: zodError });
+    this.name = "ValidationError";
+    this.issues = zodError.issues;
   }
 }
