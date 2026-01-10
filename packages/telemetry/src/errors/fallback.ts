8f00fca2193ae00bafb69efc3b4e896d07268cda
Ryan Luong <lumintriet@gmail.com>
2026-01-11 06:15:18 +0700

    feat(telemetry): allow for additional inputs to be passed to fallback errors.

--- packages/telemetry/src/errors/fallback.ts
+++ packages/telemetry/src/errors/fallback.ts
@@ -1,31 +1,38 @@
 export class UnexpectedError extends Error {
-  constructor(message: string, options?: ErrorOptions) {
+  inputs: unknown;
+
+  constructor(message: string, options?: ErrorOptions, inputs?: unknown) {
     super(message, options);
     this.name = "UnexpectedError";
+    this.inputs = inputs;
   }
 }
 
 export class ExoticError extends UnexpectedError {
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options);
+  constructor(message: string, options?: ErrorOptions, inputs?: unknown) {
+    super(message, options, inputs);
     this.name = "ExoticError";
   }
 }
 
-export function createFallbackError(error: unknown) {
+export function createFallbackError(error: unknown, inputs?: unknown) {
   if (error instanceof Error) {
-    return new UnexpectedError(error.message, { cause: error });
+    return new UnexpectedError(error.message, { cause: error }, inputs);
   }
 
   try {
-    return new ExoticError(JSON.stringify(error), { cause: error });
+    return new ExoticError(JSON.stringify(error), { cause: error }, inputs);
   } catch {
     try {
-      return new ExoticError(String(error), { cause: error });
+      return new ExoticError(String(error), { cause: error }, inputs);
     } catch {
-      return new ExoticError("Could not stringify this error", {
-        cause: error,
-      });
+      return new ExoticError(
+        "Could not stringify this error",
+        {
+          cause: error,
+        },
+        inputs
+      );
     }
   }
 }
