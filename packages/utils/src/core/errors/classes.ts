79944dadccfcb947b20793f8eb0697d04cd0a7a5
Ryan Luong <lumintriet@gmail.com>
2026-01-10 08:49:12 +0700

    feat(utils/core/errors): add HttpError class.

--- packages/utils/src/core/errors/classes.ts
+++ packages/utils/src/core/errors/classes.ts
@@ -16,3 +16,30 @@ export class ValidationError extends ExpectedError {
     this.issues = zodError.issues;
   }
 }
+
+interface HttpErrorRequest {
+  original: Request;
+  sentBody?: any;
+}
+
+interface HttpErrorResponse {
+  original: Response;
+  parsedBody?: any;
+}
+
+interface HttpErrorOptions {
+  request: HttpErrorRequest;
+  response: HttpErrorResponse;
+}
+
+export class HttpError extends ExpectedError {
+  request: HttpErrorRequest;
+  response: HttpErrorResponse;
+
+  constructor(message: string, options: HttpErrorOptions) {
+    super(message);
+    this.name = "HttpError";
+    this.request = options.request;
+    this.response = options.response;
+  }
+}
