a89fb8597e3cd2e8ec00418e390b88432f19a991
Ryan Luong <lumintriet@gmail.com>
2026-01-11 10:51:52 +0700

    draft(analyst): try using safe-fetch package.

--- services/analyst/src/utils/package-metadata/fetch.ts
+++ services/analyst/src/utils/package-metadata/fetch.ts
@@ -1,118 +1,17 @@
-import type { LogStep } from "@/contexts";
-import type {
-  ValidateInputsContext,
-  ValidInputs,
-} from "@/utils/inputs/validate";
-import { HttpError } from "@es-vanguard/utils/errors/classes";
-import { createFallbackError } from "@es-vanguard/utils/errors/fallback";
-import {
-  ABBREVIATED_METADATA_ACCEPT_HEADER,
-  NPM_REGISTRY_URL,
-} from "@es-vanguard/utils/npm-constants";
-import { err, ok } from "neverthrow";
-import { serializeError } from "serialize-error";
-
-type SerializedResponse = {
-  headers: Record<string, string>;
-  ok: Response["ok"];
-  redirected: Response["redirected"];
-  status: Response["status"];
-  statusText: Response["statusText"];
-  type: Response["type"];
-  url: Response["url"];
-};
-
-function serializeResponse(response: Response) {
-  const serializedHeaders = Object.fromEntries(response.headers.entries());
-  const serializedResponse: SerializedResponse = {
-    headers: serializedHeaders,
-    ok: response.ok,
-    redirected: response.redirected,
-    status: response.status,
-    statusText: response.statusText,
-    type: response.type,
-    url: response.url,
-  };
-
-  return serializedResponse;
-}
-
-type FetchPackageMetadataStep = LogStep<
-  "fetch-package-metadata",
-  SerializedResponse
->;
-export interface FetchPackageMetadataContext extends Omit<
-  ValidateInputsContext,
-  "steps"
-> {
-  steps: [...ValidateInputsContext["steps"], FetchPackageMetadataStep];
-}
+import { getJsonResponse } from "@es-vanguard/safe-fetch/get-json-response";
+import { NPM_REGISTRY_URL } from "@es-vanguard/utils/npm-constants";
 
 export async function fetchPackageMetadata({
-  validInputs,
-  context,
+  packageName,
 }: {
-  validInputs: ValidInputs;
-  context: ValidateInputsContext;
+  packageName: string;
 }) {
-  const startTime = Bun.nanoseconds();
-  try {
-    const url = `${NPM_REGISTRY_URL}/${validInputs.name}`;
-    const response = await fetch(url, {
-      headers: {
-        Accept: ABBREVIATED_METADATA_ACCEPT_HEADER,
-      },
-    });
-    const endTime = Bun.nanoseconds();
+  const request = new Request(`${NPM_REGISTRY_URL}/${packageName}`);
+  const getResult = await getJsonResponse(request);
 
-    const serializedResponse = serializeResponse(response);
-
-    if (!response.ok) {
-    }
-    const newContext: FetchPackageMetadataContext = {
-      ...context,
-      steps: [
-        ...context.steps,
-        {
-          name: "fetch-package-metadata",
-          time: {
-            start: startTime,
-            end: endTime,
-            duration: endTime - startTime,
-          },
-          success: true,
-          data: serializedResponse,
-        },
-      ],
-    };
-
-    return ok({
-      response,
-      context: newContext,
-    });
-  } catch (error) {
-    const endTime = Bun.nanoseconds();
-    const fallbackError = createFallbackError(error);
-    const newContext: FetchPackageMetadataContext = {
-      ...context,
-      steps: [
-        ...context.steps,
-        {
-          name: "fetch-package-metadata",
-          time: {
-            start: startTime,
-            end: endTime,
-            duration: endTime - startTime,
-          },
-          success: false,
-          error: serializeError(fallbackError),
-        },
-      ],
-    };
-
-    return err({
-      error: fallbackError,
-      context: newContext,
-    });
+  if (getResult.isErr()) {
+    throw getResult.error;
   }
+
+  return getResult.value;
 }
