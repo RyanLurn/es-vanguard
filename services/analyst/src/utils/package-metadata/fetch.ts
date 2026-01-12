93745418f42d3a3768b26a6076e8bc6a75ebd912
Ryan Luong <lumintriet@gmail.com>
2026-01-12 09:20:38 +0700

    feat(analyst): fetch package metadata util.

--- services/analyst/src/utils/package-metadata/fetch.ts
+++ services/analyst/src/utils/package-metadata/fetch.ts
@@ -1,17 +1,45 @@
-import { getJsonResponse } from "@es-vanguard/safe-fetch/get-json-response";
-import { NPM_REGISTRY_URL } from "@es-vanguard/utils/npm-constants";
+import { CustomError } from "@es-vanguard/telemetry/errors/classes";
+import { createFallbackError } from "@es-vanguard/telemetry/errors/fallback";
+import {
+  ABBREVIATED_METADATA_ACCEPT_HEADER,
+  NPM_REGISTRY_URL,
+} from "@es-vanguard/utils/npm-constants";
+import type { NpmPackageName } from "@es-vanguard/utils/npm-package-name";
+import { err, ok } from "neverthrow";
 
-export async function fetchPackageMetadata({
-  packageName,
-}: {
-  packageName: string;
-}) {
-  const request = new Request(`${NPM_REGISTRY_URL}/${packageName}`);
-  const getResult = await getJsonResponse(request);
+export async function fetchPackageMetadata({ name }: { name: NpmPackageName }) {
+  const url = `${NPM_REGISTRY_URL}/${encodeURIComponent(name)}`;
+  const fetchOptions: RequestInit = {
+    method: "GET",
+    headers: {
+      Accept: ABBREVIATED_METADATA_ACCEPT_HEADER,
+    },
+  };
+  const context = {
+    packageName: name,
+    url,
+    fetchOptions,
+  };
 
-  if (getResult.isErr()) {
-    throw getResult.error;
+  try {
+    const response = await fetch(url, fetchOptions);
+    return ok(response);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      const fetchError = new CustomError(
+        `Failed to fetch metadata for ${name} from the npm registry`,
+        {
+          cause: error,
+          expected: true,
+          code: "FETCH_ERROR",
+          context,
+        }
+      );
+
+      return err(fetchError);
+    }
+
+    const fallbackError = createFallbackError({ error, context });
+    return err(fallbackError);
   }
-
-  return getResult.value;
 }
