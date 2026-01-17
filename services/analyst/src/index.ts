3f4790ec8365a3c6cc692b641cd4558b9f08022a
Ryan Luong <lumintriet@gmail.com>
2026-01-17 07:30:10 +0700

    draft(analyst): save work.

--- services/analyst/src/index.ts
+++ services/analyst/src/index.ts
@@ -1,12 +1,16 @@
-import type { StartContext } from "@es-vanguard/telemetry/context";
+import { parseInputs } from "#utils/inputs/parse";
+import { validateInputs } from "#utils/inputs/validate";
 
-const startTime = Bun.nanoseconds();
+const parseInputsResult = await parseInputs();
+if (parseInputsResult.isErr()) {
+  console.error(parseInputsResult.error);
+  process.exit(1);
+}
+const inputs = parseInputsResult.value;
 
-const context: StartContext<"analyst"> = {
-  service: "analyst" as const,
-  runId: Bun.randomUUIDv7(),
-  time: {
-    start: startTime,
-  },
-  steps: [],
-};
+const validateInputsResult = validateInputs({ inputs });
+if (validateInputsResult.isErr()) {
+  console.error(validateInputsResult.error);
+  process.exit(1);
+}
+const validatedInputs = validateInputsResult.value;
--- services/analyst/tests/try.ts
+++ services/analyst/tests/try.ts
@@ -1,28 +1,6 @@
-import { betterFetch, ValidationError } from "@better-fetch/fetch";
-import * as z from "zod";
+const object = {
+  name: "John",
+  age: 30,
+};
 
-try {
-  const { data: todos, error: todoError } = await betterFetch(
-    "https://jsonplaceholder.typicode.com/todos/1",
-    {
-      output: z.string(),
-    }
-  );
-
-  console.log(todos);
-  console.log(todoError);
-} catch (error) {
-  console.log("Caught error:");
-  if (error instanceof z.ZodError) {
-    console.log("ZodError's issues:", error.issues);
-  } else if (error instanceof ValidationError) {
-    console.log("Validation error:");
-    console.log("Name:", error.name);
-    console.log("Message:", error.message);
-    console.log("Issues:", error.issues);
-    console.log("Stack:", error.stack);
-    console.log("Cause:", error.cause);
-  } else {
-    console.log("Unknown error:", error);
-  }
-}
+console.log(object);
